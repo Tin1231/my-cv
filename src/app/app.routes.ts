ff293bd07229da9c3c3a882b572f1ef87c0ab9ac
TinTrann1008 <tranductinn10082004@gmail.com>
2025-04-27 20:57:29 +0300

    Populate components & add basic styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
+import { AboutMeComponent }    from './about-me/about-me.component';
+import { SkillsComponent }     from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+  { path: 'about-me',   component: AboutMeComponent   },
+  { path: 'skills',     component: SkillsComponent    },
+  { path: 'experience', component: ExperienceComponent},
+  { path: '', redirectTo: 'about-me', pathMatch: 'full' }
+];
